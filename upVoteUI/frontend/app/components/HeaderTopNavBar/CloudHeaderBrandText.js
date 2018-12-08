e2ff99835406c691c84d4d7cda60593e8cebcd19
DARIUSZ CIECHANOWSKI <dariusz.ciechanowski@ibm.com>
2018-12-08 18:26:26 +0100

    Remove upVoteUI installer part

--- upVoteUI/frontend/app/components/HeaderTopNavBar/CloudHeaderBrandText.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-const CloudHeaderBrandText = styled.div`
-  font-size: 1rem;
-  font-weight: 400;
-`;
-
-export default CloudHeaderBrandText;
--- upVoteUI/frontend/app/components/HeaderTopNavBar/CloudHeaderBrandTextProduct.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-const CloudHeaderBrandTextProduct = styled.span`
-  font-size: 1rem;
-  font-weight: 600;
-`;
-
-export default CloudHeaderBrandTextProduct;
